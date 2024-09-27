6cbd89767b178dcbd001a57394969ffe53f05640
nfrgeller <69528923+nfrgeller@users.noreply.github.com>
2024-09-27 12:31:26 -0500

    changed starter icon to ascii bunny

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,13 +1,16 @@
 return {
-	-- the colorscheme should be available when starting Neovim
 	{
 		"EdenEast/nightfox.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		--options = {transparent = true,},
 		config = function()
-			-- load the colorscheme here
+			--load the colorscheme here
 			vim.cmd([[colorscheme nightfox]])
 		end,
 	},
+	-- "cocopon/iceberg.vim",
+	-- config = function()
+	-- 	vim.cmd([[colorscheme iceberg]])
+	-- end,
 }
